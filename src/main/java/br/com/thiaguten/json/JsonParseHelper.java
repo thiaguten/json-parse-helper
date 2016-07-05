8561d2fe9572ecb4ddd1b323a0a6d381637e2fdc
Thiago Gutenberg Carvalho da Costa <thiaguten@gmail.com>
2016-07-05 20:01:36 -0300

    update dependencies version, change license and improve tests

--- src/main/java/br/com/thiaguten/json/JsonParseHelper.java
+++ src/main/java/br/com/thiaguten/json/JsonParseHelper.java
@@ -1,32 +1,20 @@
-/*
+/*-
  * #%L
+ * JsonParseHelper
  * %%
- * Copyright (C) 2015 - 2016 Thiago Gutenberg Carvalho da Costa.
+ * Copyright (C) 2016 Thiago Gutenberg Carvalho da Costa
  * %%
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 
- * 3. Neither the name of the Thiago Gutenberg Carvalho da Costa. nor the names of its contributors
- *    may be used to endorse or promote products derived from this software without
- *    specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * #L%
  */
 package br.com.thiaguten.json;
@@ -46,13 +34,11 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
 /**
  * This class helps with some parse feature for converting MongoDB BSON strict/shell mode into 'standard' JSON format.
- * <p>
  * <ul>
  * <li>
  * <pre>
@@ -83,16 +69,17 @@ import java.util.regex.Pattern;
 @SuppressWarnings({"rawtypes", "unchecked"})
 public final class JsonParseHelper {
 
-    private static final String SLASH = "/";
-    private static final String BACKSLASH = "\\";
-    private static final String COMMA = ", ";
-    private static final String COLON = " : ";
-    private static final String DOUBLE_QUOTES = "\"";
-    private static final String JSON_OBJECT_START_TOKEN = "{ ";
-    private static final String JSON_OBJECT_END_TOKEN = " }";
-    private static final String JSON_ARRAY_START_TOKEN = "[ ";
-    private static final String JSON_ARRAY_END_TOKEN = " ]";
-    private static final String NULL = null;
+    public static final String SLASH = "/";
+    public static final String BACKSLASH = "\\";
+    public static final String COMMA = ", ";
+    public static final String COLON = ":";
+    public static final String DOUBLE_QUOTES = "\"";
+    public static final String JSON_OBJECT_START_TOKEN = "{";
+    public static final String JSON_OBJECT_END_TOKEN = "}";
+    public static final String JSON_ARRAY_START_TOKEN = "[";
+    public static final String JSON_ARRAY_END_TOKEN = "]";
+    public static final String NULL = null;
+    public static final int INVALID_PATTERN_FLAG = 0;
 
     private JsonParseHelper() {
         throw new AssertionError();
@@ -182,12 +169,13 @@ public final class JsonParseHelper {
     private static void checkInstance(Object value, StringBuilder builder) {
         if (value instanceof BsonValue) {
             BsonValue bsonValue = (BsonValue) value;
-            switch (bsonValue.getBsonType()) {
+            BsonType bsonType = bsonValue.getBsonType();
+            switch (bsonType) {
                 case REGULAR_EXPRESSION:
                     BsonRegularExpression bsonRegularExpression = bsonValue.asRegularExpression();
                     String pattern = BACKSLASH + bsonRegularExpression.getPattern();
                     String options = bsonRegularExpression.getOptions();
-                    String consolidate = options != null && !options.trim().isEmpty() ? pattern + SLASH + options : pattern;
+                    String consolidate = !options.trim().isEmpty() ? pattern + SLASH + options : pattern;
                     builder.append(DOUBLE_QUOTES).append(consolidate).append(DOUBLE_QUOTES);
                     break;
                 case NULL:
@@ -240,15 +228,11 @@ public final class JsonParseHelper {
             if (value instanceof String) {
                 builder.append(DOUBLE_QUOTES).append(value).append(DOUBLE_QUOTES);
             }
-            if (value instanceof Character) {
-                builder.append(DOUBLE_QUOTES).append(value).append(DOUBLE_QUOTES);
-            }
             if (value instanceof Pattern) {
-
                 Pattern pattern = ((Pattern) value);
-                String p = BACKSLASH + pattern.pattern();
-                String flags = PatternFlag.of(pattern.flags()).getFlagAsString();
-                String consolidate = flags != null && !flags.trim().isEmpty() ? p + SLASH + flags : p;
+                String patternValue = BACKSLASH + pattern.pattern();
+                int flags = pattern.flags();
+                String consolidate = flags > INVALID_PATTERN_FLAG ? patternValue + SLASH + PatternFlag.of(flags).getFlagAsString() : patternValue;
 
                 builder.append(DOUBLE_QUOTES).append(consolidate).append(DOUBLE_QUOTES);
             }
@@ -256,9 +240,6 @@ public final class JsonParseHelper {
                 builder.append(value);
             }
             if (value instanceof Boolean) {
-                builder.append(value);
-            }
-            if (value instanceof AtomicBoolean) {
                 builder.append(value);
             }
             if (value == null) {
@@ -301,6 +282,7 @@ public final class JsonParseHelper {
         public String getFlagAsString() {
             return flagAsString;
         }
+
     }
 
 }
--- src/test/java/br/com/thiaguten/json/JsonParseHelperTest.java
+++ src/test/java/br/com/thiaguten/json/JsonParseHelperTest.java
@@ -1,32 +1,20 @@
-/*
+/*-
  * #%L
+ * JsonParseHelper
  * %%
- * Copyright (C) 2015 - 2016 Thiago Gutenberg Carvalho da Costa.
+ * Copyright (C) 2016 Thiago Gutenberg Carvalho da Costa
  * %%
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 
- * 3. Neither the name of the Thiago Gutenberg Carvalho da Costa. nor the names of its contributors
- *    may be used to endorse or promote products derived from this software without
- *    specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * #L%
  */
 package br.com.thiaguten.json;
@@ -37,25 +25,109 @@ import org.bson.json.JsonWriterSettings;
 import org.bson.types.ObjectId;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.*;
 import java.util.regex.Pattern;
 
 import static br.com.thiaguten.json.JsonParseHelper.parse;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 public class JsonParseHelperTest {
 
     private static final Document document = new Document()
             .append("_id", new ObjectId("5662e5798172910f5a925a43"))
-//            .append("date", new Date(1449321983184L))
+            .append("date", new Date(1449321983184L))
             .append("pattern", Pattern.compile("\\d", Pattern.CASE_INSENSITIVE))
-            .append("long", Long.MAX_VALUE);
+            .append("pattern2", Pattern.compile("\\s"))
+            .append("long", Long.MAX_VALUE)
+            .append("null", null)
+            .append("double", 1.0)
+            .append("string", "thiago")
+            .append("boolean", true)
+            .append("doc", new Document("key", "value"))
+            .append("list", Arrays.asList("value"))
+            .append("map", Collections.singletonMap("key", "value"));
 
     private static final BsonDocument bsonDocument = new BsonDocument()
             .append("_id", new BsonObjectId(new ObjectId("5662e5798172910f5a925a43")))
-//            .append("date", new BsonDateTime(1449321983184L))
+            .append("date", new BsonDateTime(1449321983184L))
             .append("pattern", new BsonRegularExpression("\\d", "i"))
-            .append("long", new BsonInt64(Long.MAX_VALUE));
+            .append("pattern2", new BsonRegularExpression("\\s"))
+            .append("long", new BsonInt64(Long.MAX_VALUE))
+            .append("null", new BsonNull())
+            .append("double", new BsonDouble(1.0))
+            .append("string", new BsonString("thiago"))
+            .append("boolean", new BsonBoolean(true))
+            .append("doc", new BsonDocument("key", new BsonString("value")))
+            .append("list", new BsonArray(Arrays.asList(new BsonString("value"))))
+            .append("map", new BsonDocument(Arrays.asList(new BsonElement("key", new BsonString("value")))));
+
+    @Test
+    public void constantsTest() {
+        assertEquals("/", JsonParseHelper.SLASH);
+        assertEquals("\\", JsonParseHelper.BACKSLASH);
+        assertEquals(", ", JsonParseHelper.COMMA);
+        assertEquals(":", JsonParseHelper.COLON);
+        assertEquals("\"", JsonParseHelper.DOUBLE_QUOTES);
+        assertEquals("{", JsonParseHelper.JSON_OBJECT_START_TOKEN);
+        assertEquals("}", JsonParseHelper.JSON_OBJECT_END_TOKEN);
+        assertEquals("[", JsonParseHelper.JSON_ARRAY_START_TOKEN);
+        assertEquals("]", JsonParseHelper.JSON_ARRAY_END_TOKEN);
+        assertEquals(null, JsonParseHelper.NULL);
+        assertEquals(0, JsonParseHelper.INVALID_PATTERN_FLAG);
+    }
+
+    @Test
+    public void illegalInstatiationUtillityClass() throws IllegalAccessException, InstantiationException, NoSuchMethodException {
+        try {
+            Constructor c = JsonParseHelper.class.getDeclaredConstructor();
+            assertTrue(Modifier.isPrivate(c.getModifiers()));
+            c.setAccessible(true);
+            c.newInstance();
+        } catch (InvocationTargetException e) {
+            assertThat(e.getCause(), is(instanceOf(AssertionError.class)));
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void patternTest() {
+        JsonParseHelper.PatternFlag.of(JsonParseHelper.INVALID_PATTERN_FLAG);
+    }
+
+    @Test
+    public void parseStringTest() {
+        String json = "[1, \"string\", true]";
+        assertEquals(json, parse(json));
+        json = null;
+        assertEquals(null, parse(json));
+        json = "";
+        assertEquals(json, parse(json));
+    }
+
+    @Test
+    public void parseMapTest() {
+        String empty = "{}";
+        String notEmpty = "{\"key\":\"value\"}";
+        assertEquals(empty, JsonParseHelper.parse((Map) null));
+        assertEquals(empty, JsonParseHelper.parse(Collections.emptyMap()));
+        assertEquals(notEmpty, parse(Collections.singletonMap("key", "value")));
+        assertEquals(notEmpty, parse(new Document("key", "value")));
+        assertEquals(notEmpty, parse(new BsonDocument(Arrays.asList(new BsonElement("key", new BsonString("value"))))));
+    }
+
+    @Test
+    public void parseListTest() {
+        String empty = "[]";
+        String notEmpty = "[\"value\"]";
+        assertEquals(empty, JsonParseHelper.parse((List) null));
+        assertEquals(empty, parse(Collections.emptyList()));
+        assertEquals(notEmpty, parse(Arrays.asList("value")));
+        assertEquals(notEmpty, parse(new BsonArray(Arrays.asList(new BsonString("value")))));
+    }
 
     @Test
     public void bsonArrayTest() {
@@ -74,43 +146,43 @@ public class JsonParseHelperTest {
         bsonArray3.add(new BsonArray(bsonArray2));
 
         assertNotNull(bsonArray3);
-        assertEquals("[ \"a\", \"b\", [ \"c\", \"d\", [ \"e\", \"f\" ] ] ]", parse(bsonArray3));
+        assertEquals("[\"a\", \"b\", [\"c\", \"d\", [\"e\", \"f\"]]]", parse(bsonArray3));
     }
 
     @Test
     public void bsonDocumentTest() {
         assertNotNull(bsonDocument);
-        assertEquals("{ \"_id\" : \"5662e5798172910f5a925a43\", \"pattern\" : \"\\\\d/i\", \"long\" : 9223372036854775807 }", parse(bsonDocument));
+        assertEquals("{\"_id\":\"5662e5798172910f5a925a43\", \"date\":\"2015-12-05T11:26:23.184\", \"pattern\":\"\\\\d/i\", \"pattern2\":\"\\\\s\", \"long\":9223372036854775807, \"null\":null, \"double\":1.0, \"string\":\"thiago\", \"boolean\":true, \"doc\":{\"key\":\"value\"}, \"list\":[\"value\"], \"map\":{\"key\":\"value\"}}", parse(bsonDocument));
     }
 
     @Test
     public void bsonDocumentStrictTest() {
         assertNotNull(bsonDocument);
-        assertEquals("{ \"_id\" : \"5662e5798172910f5a925a43\", \"pattern\" : \"\\\\d/i\", \"long\" : 9223372036854775807 }", parse(bsonDocument.toJson()));
+        assertEquals("{\"_id\":\"5662e5798172910f5a925a43\", \"date\":\"2015-12-05T11:26:23.184\", \"pattern\":\"\\\\d/i\", \"pattern2\":\"\\\\s\", \"long\":9223372036854775807, \"null\":null, \"double\":1.0, \"string\":\"thiago\", \"boolean\":true, \"doc\":{\"key\":\"value\"}, \"list\":[\"value\"], \"map\":{\"key\":\"value\"}}", parse(bsonDocument.toJson()));
     }
 
     @Test
     public void bsonDocumentShellTest() {
         assertNotNull(bsonDocument);
-        assertEquals("{ \"_id\" : \"5662e5798172910f5a925a43\", \"pattern\" : \"\\\\d/i\", \"long\" : 9223372036854775807 }", parse(bsonDocument.toJson(new JsonWriterSettings(JsonMode.SHELL))));
+        assertEquals("{\"_id\":\"5662e5798172910f5a925a43\", \"date\":\"2015-12-05T11:26:23.184\", \"pattern\":\"\\\\d/i\", \"pattern2\":\"\\\\s\", \"long\":9223372036854775807, \"null\":null, \"double\":1.0, \"string\":\"thiago\", \"boolean\":true, \"doc\":{\"key\":\"value\"}, \"list\":[\"value\"], \"map\":{\"key\":\"value\"}}", parse(bsonDocument.toJson(new JsonWriterSettings(JsonMode.SHELL))));
     }
 
     @Test
     public void documentTest() {
         assertNotNull(document);
-        assertEquals("{ \"_id\" : \"5662e5798172910f5a925a43\", \"pattern\" : \"\\\\d/i\", \"long\" : 9223372036854775807 }", parse(document));
+        assertEquals("{\"_id\":\"5662e5798172910f5a925a43\", \"date\":\"2015-12-05T11:26:23.184\", \"pattern\":\"\\\\d/i\", \"pattern2\":\"\\\\s\", \"long\":9223372036854775807, \"null\":null, \"double\":1.0, \"string\":\"thiago\", \"boolean\":true, \"doc\":{\"key\":\"value\"}, \"list\":[\"value\"], \"map\":{\"key\":\"value\"}}", parse(document));
     }
 
     @Test
     public void documentStrictTest() {
         assertNotNull(document);
-        assertEquals("{ \"_id\" : \"5662e5798172910f5a925a43\", \"pattern\" : \"\\\\d/i\", \"long\" : 9223372036854775807 }", parse(document.toJson(new JsonWriterSettings(JsonMode.STRICT))));
+        assertEquals("{\"_id\":\"5662e5798172910f5a925a43\", \"date\":\"2015-12-05T11:26:23.184\", \"pattern\":\"\\\\d/i\", \"pattern2\":\"\\\\s\", \"long\":9223372036854775807, \"null\":null, \"double\":1.0, \"string\":\"thiago\", \"boolean\":true, \"doc\":{\"key\":\"value\"}, \"list\":[\"value\"], \"map\":{\"key\":\"value\"}}", parse(document.toJson(new JsonWriterSettings(JsonMode.STRICT))));
     }
 
     @Test
     public void documentShellTest() {
         assertNotNull(document);
-        assertEquals("{ \"_id\" : \"5662e5798172910f5a925a43\", \"pattern\" : \"\\\\d/i\", \"long\" : 9223372036854775807 }", parse(document.toJson(new JsonWriterSettings(JsonMode.SHELL))));
+        assertEquals("{\"_id\":\"5662e5798172910f5a925a43\", \"date\":\"2015-12-05T11:26:23.184\", \"pattern\":\"\\\\d/i\", \"pattern2\":\"\\\\s\", \"long\":9223372036854775807, \"null\":null, \"double\":1.0, \"string\":\"thiago\", \"boolean\":true, \"doc\":{\"key\":\"value\"}, \"list\":[\"value\"], \"map\":{\"key\":\"value\"}}", parse(document.toJson(new JsonWriterSettings(JsonMode.SHELL))));
     }
 
 }
